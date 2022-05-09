22307ac86bd3dcfeeb53afe9abc7565ececd9d08
kolonyavo <hei.kolo.17@gmail.com>
2022-05-09 08:28:16 +0300

    declaration d'un nouveau tableau listCard

--- missingCards.js
+++ missingCards.js
@@ -2,8 +2,9 @@ let values = ['2','3','4','5','6','7','8','9','10','V','D','R','A']
 let colors = ['C', 'P', 'Q', 'T']
 
 function listCards(values, colors) {
-    
+    let listCard = [];
 }
+
 function missingCards(input) {
     
 }
